3b68876818b9a86cf89ebb5afc1e343f88ebea91
kara-o <44376276+kara-o@users.noreply.github.com>
2019-11-20 15:32:25 -0800

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :password, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :first_name, presence: true 
+  validates :last_name, presence: true
 end
