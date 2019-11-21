1f57b800429f7462025cb4355486925a28ed0d7f
kara-o <44376276+kara-o@users.noreply.github.com>
2019-11-21 14:55:31 -0800

    updated auth controller for verifying current_user

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::AuthController < ApplicationController
-  # skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:create]
  
   def create
-    puts user_login_params
     user = User.find_by(username: user_login_params[:username])
     if user && user.authenticate(user_login_params[:password])
       token = encode_token({ user_id: user.id })
@@ -11,6 +10,10 @@ class Api::V1::AuthController < ApplicationController
       render json: { error: 'invalid username or password' }, status: :unauthorized
     end
   end
+
+  def show
+    render json: { user: current_user }
+  end 
  
   private
  
