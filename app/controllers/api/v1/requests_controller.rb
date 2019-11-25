117cd19650af0aac1ae9603133e39d7203ec5702
kara-o <44376276+kara-o@users.noreply.github.com>
2019-11-24 18:01:53 -0800

    working on create request

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -1,7 +1,26 @@
 class Api::V1::RequestsController < ApplicationController
 
   def create
-  byebug
+    if current_user.id == params[:user_id].to_i
+      request = Request.create(request_params)
+      if request.valid?
+        render json: { request: request }, status: :created
+      else 
+        render json: { errors: { error_obj: request.errors.messages, full_messages: request.errors.full_messages} }, status: :not_acceptable
+      end 
+    else 
+      byebug
+      render json: { message: 'User ids do not match' }
+    end 
+    
+
+
+  end 
+
+  private
+
+  def request_params
+    params.require(:request).permit(:date, :start_time, :end_time, :cuisine, :neighborhood, :price, :user_id)
   end 
 
 end
