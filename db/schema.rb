c474659a9a90542637640f0f775d9f4d7fdef9c3
Kara Odle <karajo.odle@gmail.coml>
2019-11-29 11:35:44 -0800

    saving before switch to devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_004713) do
+ActiveRecord::Schema.define(version: 2019_11_27_223207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_004713) do
     t.integer "itinerary_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "status", default: "pending"
+    t.string "status", default: "requested"
   end
 
   create_table "users", force: :cascade do |t|
