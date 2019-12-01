1c743c5ab822938f3651a55b757113e882a72dcb
kara-o <44376276+kara-o@users.noreply.github.com>
2019-12-01 13:11:46 -0800

    auth login now working

--- app/controllers/api/v1/prices_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::PricesController < ApplicationController
-
-  def index
-    prices = Price.all  
-    render json: prices 
-  end 
-
-end
--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,5 +1,5 @@
 class RequestSerializer < ActiveModel::Serializer
-  attributes :id, :start_time, :end_time, :party_size, :neighborhood, :price_range, :created_at, :updated_at, :fulfilled, :cancelled, :user, :notes
+  attributes :id, :start_time, :end_time, :party_size, :neighborhood_id, :price_range_id, :user_id, :created_at, :updated_at, :fulfilled, :cancelled, :notes
 
   def created_at
     self.object.created_at.strftime('Created on %m/%d/%Y at %l:%M%p')
@@ -7,10 +7,5 @@ class RequestSerializer < ActiveModel::Serializer
 
   def updated_at
     self.object.updated_at.strftime('Edited on %m/%d/%Y at %l:%M%p')
-  end 
-
-  def user
-    user = User.find(self.object.user_id)
-    UserSerializer.new(user)
   end 
 end
