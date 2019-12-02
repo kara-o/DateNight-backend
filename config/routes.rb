c0020b549cb47ca837d6f6e4a8d87d1e2926bc41
Kara Odle <karajo.odle@gmail.coml>
2019-12-01 21:19:03 -0800

    working on admin auth - added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
-    mount_devise_token_auth_for 'Admin', at: 'admin_auth'
+    mount_devise_token_auth_for 'Admin', at: 'admin_auth', skip: [:omniauth_callbacks]
 
     namespace :api do
       namespace :v1 do
@@ -10,6 +10,13 @@ Rails.application.routes.draw do
           #   resources :requests, only:[:index, :show, :update]
           #   resources :itinerary_items, only: [:create]
           # end
+        devise_scope :admin do
+          get '/users', to: 'users#index'
+          get '/requests', to: 'requests#index'
+          get '/requests/:id', to: 'requests#show'
+          post '/requests/:id', to: 'requests#update'
+          get '/itinerary_items', to: 'itinerary_items#create'
+        end
     
         resources :users, only: [:create] do
           resources :requests, only: [:create, :index, :show, :update] do
