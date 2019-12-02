77596694fcf0917c88e000f60a01e6a7e7b66052
Kara Odle <karajo.odle@gmail.coml>
2019-12-02 13:46:44 -0800

    added price_range to request serializer

--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,9 +1,13 @@
 class RequestSerializer < ActiveModel::Serializer
-  attributes :id, :start_time, :end_time, :party_size, :neighborhood, :price_range_id, :user_id, :created_at, :updated_at, :fulfilled, :cancelled, :notes, :contacts
+  attributes :id, :start_time, :end_time, :party_size, :neighborhood, :price_range, :user_id, :created_at, :updated_at, :fulfilled, :cancelled, :notes, :contacts
 
   def neighborhood
     Neighborhood.find(self.object.neighborhood_id).name
   end
+
+  def price_range
+    PriceRange.find(self.object.price_range_id).max_amount
+  end 
 
   def created_at
     self.object.created_at.strftime('Created on %m/%d/%Y at %l:%M%p')
