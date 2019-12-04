3bee8f681ae9ae25d48ca608e45a562c0156ac20
Kara Odle <karajo.odle@gmail.coml>
2019-12-04 12:05:18 -0800

    initial seed data for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,30 @@
 neighborhoods = [
-  'Capitol Hill / First Hill',
+  'Capitol Hill',
   'Downtown',
-  'International District',
-  'Waterfront / Belltown',
-  'Lake Union / Eastlake',
-  'Leschi / Madrona / Madison Park',
+  'South Lake Union',
   'Queen Anne / Magnolia',
   'West Seattle',
   'U District',
-  'Pioneer Square',
-  'Fremont / Wallingford',
+  'Ballard / Fremont / Wallingford',
   'Greenwood / Greenlake / Phinney Ridge',
-  'Ballard',
-  'Columbia City / Beacon Hill'
 ]
 
 prices = [
-  '$',
-  '$$',
-  '$$$',
-  '$$$$'
+  '$ ( $30 and under per person ) ',
+  '$$ ( $31 to $50 per person ) ',
+  '$$$ ( $51 to $100 per person )',
+  '$$$$ ( Over $100 per person )'
 ]
 
 Admin.destroy_all
 User.destroy_all
+Contact.destroy_all
 Request.destroy_all
 Neighborhood.destroy_all
 PriceRange.destroy_all
+ItineraryItem.destroy_all
+ItineraryPackage.destroy_all
+ItineraryPackageItem.destroy_all
 
 neighborhoods.each do |neighborhood|
   Neighborhood.create(name: neighborhood)
@@ -36,4 +34,6 @@ prices.each do |price|
   PriceRange.create(max_amount: price)
 end
 
-Admin.create(email: 'karajo.odle@gmail.com', password: '123456')
+Admin.create(email: 'admin@datenight.com', password: '123')
+
+User.create(email: 'karajo.odle@gmail.com', password: '123', confirmed_at: '2019-12-04 06:32:55.476764', first_name: 'Kara', last_name: 'Odle', phone: '2069102789')
