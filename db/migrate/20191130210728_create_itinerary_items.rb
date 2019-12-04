7886b68a9d23b6db2ca1c0cdc9f1c1dbd87ef61c
Kara Odle <karajo.odle@gmail.coml>
2019-12-03 19:10:34 -0800

    working itin pkg times

--- db/migrate/20191130210728_create_itinerary_items.rb
+++ db/migrate/20191130210728_create_itinerary_items.rb
@@ -2,7 +2,8 @@ class CreateItineraryItems < ActiveRecord::Migration[6.0]
   def change
     create_table :itinerary_items do |t|
       t.integer :request_id
-      t.integer :time
+      t.datetime :arrival_time
+      t.integer :duration
       t.string :address
       t.string :place
       t.text :blurb
