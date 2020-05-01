20a270fd866789a715480b9aa011e4840e68c237
kara-o <44376276+kara-o@users.noreply.github.com>
2020-05-01 15:28:00 -0700

    added admin_addressed_cancel column to requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_003229) do
+ActiveRecord::Schema.define(version: 2020_05_01_214932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 2020_04_11_003229) do
     t.boolean "cancelled", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "admin_addressed_cancel"
   end
 
   create_table "reviews", force: :cascade do |t|
